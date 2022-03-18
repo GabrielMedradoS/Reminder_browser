dead8dc6054a4283cbf6fd213a0acd6af0a01488
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2022-03-18 20:09:49 -0300

    closure

--- src/App.js
+++ src/App.js
@@ -2,21 +2,18 @@ import { Notifier } from "./Notifier.js";
 import { Timer } from "./Timer.js";
 import { Emitter } from "./Emitter.js";
 
+const notify = Notifier.notify({
+  title: "Hora do Estudo",
+  body: "Organize seu tempo e estude",
+});
+
 const App = {
   async start() {
     try {
       await Notifier.init();
 
-      Emitter.on("countdown-start", () => {
-        Notifier.notify({
-          title: "Hora do Estudo",
-          body: "Organize seu tempo e estude",
-        });
-      });
-
-      Emitter.on("countdown-end", () => {
-        Timer.init();
-      });
+      Emitter.on("countdown-start", notify);
+      Emitter.on("countdown-end", Timer.init);
 
       Timer.init(25 * 60);
     } catch (err) {
