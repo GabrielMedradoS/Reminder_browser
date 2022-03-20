add9ef70d0436fcd1dd0969a5f505d26785ff9fc
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2022-03-20 00:24:43 -0300

    Readme

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const { app, BrowserWindow } = require("electron");
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 250,
+    height: 310,
+    // frame: false
   });
 
   win.loadFile("index.html");
