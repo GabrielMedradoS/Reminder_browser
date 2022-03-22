e121ce82adf0389e9d8c0efbdb8b8b5b36f42272
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2022-03-22 14:27:31 -0300

    control window

--- electron/CreateWindow.js
+++ electron/CreateWindow.js
@@ -3,11 +3,11 @@ const { BrowserWindow } = require("electron");
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 270,
-    height: 310,
-    //show: false,
-    //frame: false,
-    // resizable: false, // - para n redefinir o tamanho da tela
-    // fullscreenable: false, // - para n ativar modo full screen
+    height: 280,
+    show: false, // nao aparecer no meio da tela ao iniciar
+    // frame: false, // aba superior (min, max, close)
+    resizable: false, // - para n redefinir o tamanho da tela
+    fullscreenable: false, // - para n ativar modo full screen
   });
 
   win.loadFile("index.html");
